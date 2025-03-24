5ae1aa333a22d51035e710d99905da3fec262119
Jeevit-G <jeevitgupta@riccarton.school.nz>
2025-03-25 09:23:02 +1300

    it now starts to give outputs but doesn't print out the numbers

--- Internal Assesment/python.py
+++ Internal Assesment/python.py
@@ -5,18 +5,17 @@
 speed = 0 
 speed_list = []
 safespeed = 10
-stop = 'final'
+stop = 'Final'
 
 #Asking for Input
-DS = float(input("Input descent speed in m/s: "))
-while speed != stop:
+DS = input("Input descent speed in m/s: ")
+while DS != stop:
     try:
-        DS = DS.lower()
         if DS.isnumeric():
             speed_list.append(int(DS)) #Putting user input into empty list
         else:
             print('Invalid score!')
-        DS = float(input("Input descent speed in m/s: "))
+        DS = input("Input descent speed in m/s: ")
     except ValueError:
         print('Invalid score!')
         
@@ -25,5 +24,5 @@ while speed != stop:
 for item in speed_list:
     if item > 10:
         speed = speed + 1
-if speed == 1: #output
+if speed > 1: #output
     print(f'There were {speed} satelites faster than the safe speed')
