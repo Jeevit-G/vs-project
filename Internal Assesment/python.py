6c571a6312c58e3ea5e4430a77d7831e3390b7aa
Jeevit-G <jeevitgupta@riccarton.school.nz>
2025-03-28 13:52:11 +1300

    final code

--- Internal Assesment/python.py
+++ Internal Assesment/python.py
@@ -3,42 +3,34 @@
  
 # These are the variables used in the program
 speed = 0 
+mini = 0
 speed_list = [] # Creates an empty list
-high_list = []
-safespeed = 10
+max = 10
 stop = 'final' # Used to end the code
 
 #Asking for Input
-word = input("Input descent speed in m/s: ").lower()
-
+word = input("Input descent speed in m/s: ")
 while word != stop:
     try:
+        word = word.lower()
         if word.replace(".", "").isnumeric(): #Checks if the input is a numeric if float is replaces the decimal with blank
             speed_list.append(float(word)) #Putting user input into empty list and floats it
         else:
             print('Error, invalid input.') # If not numeric it prints this and asks to try again
-        word = input("Input descent speed in m/s: ").lower()
+        word = input("Input descent speed in m/s: ")
+        word = word.lower()
     except ValueError:
         print('Error, invalid input.') # If error it prints
  
  #This block of program prints out the results
  
 for item in speed_list:
-    if item > safespeed:
+    if item > max:
         speed = speed + 1
-        high_list.append(float(item))
- 
-if speed > 1: #output
-    print(f'There were {speed} satellites faster than the safe speed.') # Plural 
+print(f'There were {speed} satellites faster than the safe speed.') 
+if speed > mini: #output
     print('The unsafe speeds are')
-    for n in high_list:
-        print(n)
- 
-elif speed == 1: 
-    print(f'There was {speed} satellite faster than the safe speed.')  # Non Plural
-    print('The unsafe speed is')
-    for n in high_list:
-        print(n)
- 
-else:
-    print(f'There were {speed} satellite faster than the safe speed.')
+    for item in speed_list:
+        if item > max:
+            print(item)
+
